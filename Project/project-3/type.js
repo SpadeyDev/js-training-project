73a09776e3f20c75a84e5ff9284f2214606a2bcd
Spadey <69716874+SpadeyDev@users.noreply.github.com>
2023-09-02 00:33:43 +0300

    Update type.js

--- Project/project-3/type.js
+++ Project/project-3/type.js
@@ -1,1 +1,12 @@
+    
+    const h1 = document.querySelector('h1')
+    const h2 = document.querySelector('h2')
 
+    
+    document.body.addEventListener('keydown', e => {
+    h1.innerHTML=`Şu tuşa bastın: <span class="key">${e.key}<span>`
+    h2.textContent= `${e.keyCode}`
+})
+
+   keyConveyer.addEventListener('transitionend', (e)=>{
+  })
